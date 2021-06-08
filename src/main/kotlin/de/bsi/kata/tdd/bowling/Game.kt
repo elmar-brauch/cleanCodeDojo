c82b0442c822652683d03d91fc4f9b126157c5b7
Elmar Brauch <elmar.brauch@gmail.com>
2021-06-08 14:18:56 +0200

    TDD: Compile errors due to previous Unit test extension fixed. Unit test extended, so that it fails due to compile error.

--- src/main/kotlin/de/bsi/kata/tdd/bowling/Game.kt
+++ src/main/kotlin/de/bsi/kata/tdd/bowling/Game.kt
@@ -2,4 +2,8 @@ package de.bsi.kata.tdd.bowling
 
 class Game {
 
+    fun role(pins: Int) {
+
+    }
+
 }
--- src/test/kotlin/de/bsi/kata/tdd/bowling/GameTest.kt
+++ src/test/kotlin/de/bsi/kata/tdd/bowling/GameTest.kt
@@ -1,5 +1,6 @@
 package de.bsi.kata.tdd.bowling
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class GameTest {
@@ -8,6 +9,7 @@ class GameTest {
     fun bowlingGame() {
         val game = Game()
         game.role(0)
+        assertEquals(0, game.score())
     }
 
 }
