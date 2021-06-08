705fd4f7b8d42a2b3cb66d3987229549c67ab08e
Elmar Brauch <elmar.brauch@gmail.com>
2021-06-08 16:09:32 +0200

    TDD: Fix: Only Strikes Final refactorings

--- src/main/kotlin/de/bsi/kata/tdd/bowling/Game.kt
+++ src/main/kotlin/de/bsi/kata/tdd/bowling/Game.kt
@@ -1,5 +1,8 @@
 package de.bsi.kata.tdd.bowling
 
+const val LAST_FRAME = 18
+const val ALL_PINS = 10
+
 class Game {
 
     private val pinsRolled = IntArray(21)
@@ -9,14 +12,14 @@ class Game {
         if (isOver())
             throw IllegalStateException("Game is over.")
         pinsRolled[currentRole] = pins
-        if (isStrike(currentRole))
+        if (isStrike(currentRole) && currentRole < LAST_FRAME)
             currentRole++
         currentRole++
     }
 
     fun score(): Int {
-        var sum = pinsRolled[18] + pinsRolled[19] + pinsRolled[20]
-        for (role in 0..17) {
+        var sum = pinsRolled.filterIndexed { index, _ -> index >= LAST_FRAME }.sum()
+        for (role in 0 until LAST_FRAME) {
             sum += pinsRolled[role]
             if (isSpare(role))
                 sum += pinsRolled[role+2]
@@ -26,11 +29,11 @@ class Game {
         return sum
     }
 
-    private fun isOver() = currentRole > 20 || currentRole == 20 && !(isSpare(18) || isStrike(18))
+    private fun isOver() = currentRole > 20 || currentRole == 20 && !(isSpare(LAST_FRAME) || isStrike(LAST_FRAME))
 
-    private fun isSpare(role: Int) = role % 2 == 0 && pinsRolled[role] < 10 &&
-            pinsRolled[role] + pinsRolled[role + 1] == 10
+    private fun isSpare(role: Int) = role % 2 == 0 && pinsRolled[role] < ALL_PINS &&
+            pinsRolled[role] + pinsRolled[role + 1] == ALL_PINS
 
-    private fun isStrike(role: Int) = role % 2 == 0 && pinsRolled[role] == 10
+    private fun isStrike(role: Int) = role % 2 == 0 && pinsRolled[role] == ALL_PINS
 
 }
